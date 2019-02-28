d88a1c2eee916dcfcef3b714dea7c54777e3ddc4
Euan <esutherland@live.co.uk>
2019-02-28 22:59:12 +0000

    65. Created unit tests for the method in app.java that prints out edinburgh details.

---
+++ src/test/java/com/napier/sem/AppTest.java
@@ -0,0 +1,44 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+    }
+
+    @Test
+    void printEdinburghPopNull()
+    {
+        app.cityPopulationEdinburgh(null);
+    }
+
+    @Test
+    void printEdinburghPopTestEmpty()
+    {
+        City city = new City();
+        app.cityPopulationEdinburgh(city);
+    }
+
+    @Test
+    void printCity()
+    {
+        City city = new City();
+        city.name = "Edinburgh";
+        city.country = "United Kingdom";
+        city.district = "Scotland";
+        city.population = 500500;
+        app.cityPopulationEdinburgh(city);
+    }
+}
